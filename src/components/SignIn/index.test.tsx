598096ac80c3aa9878ab55cb9bd37f2c0854ec80
niyaraarinda3 <niyaraarinda3@gmail.com>
2022-07-20 20:03:44 +0700

    fix signin

--- src/components/SignIn/index.test.tsx
+++ src/components/SignIn/index.test.tsx
@@ -1,24 +1,25 @@
-import { render, screen, cleanup, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
-import SignIn from '../Signin';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { render, screen, cleanup, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import SignIn from "../SignIn";
+import { BrowserRouter as Router } from "react-router-dom";
 
 describe("Sign In Form", () => {
   const onSubmit = jest.fn();
   const onChange = jest.fn();
-  const emailValue = ""
-  const passwordValue = ""
+  const emailValue = "";
+  const passwordValue = "";
 
-  const view = () => render(
-    <Router>
-      <SignIn
-        onSubmit={onSubmit}
-        onChange={onChange}
-        emailValue={emailValue}
-        passwordValue={passwordValue}
-      />
-    </Router>
-  );
+  const view = () =>
+    render(
+      <Router>
+        <SignIn
+          onSubmit={onSubmit}
+          onChange={onChange}
+          emailValue={emailValue}
+          passwordValue={passwordValue}
+        />
+      </Router>
+    );
 
   afterEach(cleanup);
 
@@ -27,15 +28,14 @@ describe("Sign In Form", () => {
     const emailInput = screen.getByPlaceholderText(/Email/i);
 
     expect(emailInput).toBeInTheDocument();
-  })
+  });
 
   it("email input can be typed", () => {
     view();
     const emailInput = screen.getByPlaceholderText(/Email/i);
-    const email = "renaldkarel@gmail.com"
+    const email = "renaldkarel@gmail.com";
 
     userEvent.type(emailInput, email);
-    waitFor(() =>
-      expect(emailInput).toHaveValue(email));
-  })
+    waitFor(() => expect(emailInput).toHaveValue(email));
+  });
 });
